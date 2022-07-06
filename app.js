fbe8b96c72bf67445b4a455217890e5580c3fd80
Santiago Gutierrez <santigutierrez0001@gmail.com>
2022-07-06 11:44:32 -0500

    declarando o llamando las librerias necesarias en app.js

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const res = require('express/lib/response');
+const bcryptjs = require('bcryptjs');
+const session = require('express-session');
 const conection = require('./database/db');
+const { resetWatchers } = require('nodemon/lib/monitor/watch');
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use('/resources', express.static('public'));
+app.use('/responces', express.static(__dirname + '/public'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
 
 app.listen(5000, (req, res) => {
     console.log("Servidor corriendo en https://localhost:5000");
