542f6e962b7dc29d42befeb5ea1e6f2127207746
Santiago Gutierrez <santigutierrez0001@gmail.com>
2022-07-07 10:50:49 -0500

    frontend y backend del login terminado

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use(session({
 
 app.set('view engine', 'ejs');
 
-// RUTAS
+    // LOGIN
 app.get('/login', (req, res) => {
     res.render('login/login');
 });
-
+//post registrar
 app.post('/registrar', async (req, res) => {
     const nombre = req.body.nombre;
     const apellido = req.body.apellido;
@@ -30,7 +30,7 @@ app.post('/registrar', async (req, res) => {
     const user = req.body.username;
     const pass = req.body.pass;
     let passwordHassh = await bcryptjs.hash(pass, 8);
-    conection.query("INSERT INTO usuarios SET ?", {nombre:nombre, apellido:apellido, usuario:user, pass:passwordHassh}, async(error, result) => {
+    conection.query("INSERT INTO usuarios SET ?", {nombre:nombre, apellido:apellido, usuario:user, pass:passwordHassh, rol:rol}, async(error, result) => {
         if (error){
             console.log(error);
         } else {
@@ -44,6 +44,71 @@ app.post('/registrar', async (req, res) => {
                 ruta: 'login'
             });
         }
+    });
+});
+
+//post login
+app.post('/auth', async (req, res) => {
+    const user = req.body.username;
+    const pass = req.body.pass;
+    let passwordHassh = await bcryptjs.hash(pass, 8);
+    if (user && pass) {
+        conection.query('SELECT * FROM usuarios WHERE usuario = ?', [user], async (error, results) => {
+            if (results.lenght == 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+                res.render('login/login', {
+                    alert: true,
+                    alertTitle: "error",
+                    alertMessage: "Usuario y/o Contraseña incorrecta",
+                    alertIcon: "error",
+                    showConfirmButton: true,
+                    timer: 2000,
+                    ruta: 'login'
+                });
+            } else {
+                req.session.loggedin = true;
+                req.session.name = results[0].nombre;
+                res.render('login/login', {
+                    alert: true,
+                    alertTitle: "Bienvenido",
+                    alertMessage: "Login Correcto",
+                    alertIcon: "success",
+                    showConfirmButton: true,
+                    timer: 2000,
+                    ruta: '/'
+                })
+            }
+        });
+    } else {
+        res.render('login/login', {
+            alert: true,
+            alertTitle: "Advertencia",
+            alertMessage: "Por favor ingrese un Usuario y/o Constraseña",
+            alertIcon: "warning",
+            showConfirmButton: true,
+            timer: 2000,
+            ruta: 'login'
+        });
+    }
+});
+
+// control de Session
+app.get('/', (req, res) => {
+    if(req.session.loggedin){
+        res.render('index', {
+            login: true,
+            name: req.session.name
+        });
+    }else{
+        res.render('index', {
+            login: false,
+            name: 'iniciar session'
+        });
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
     });
 });
 
