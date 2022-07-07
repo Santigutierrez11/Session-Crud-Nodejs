df272de7f7f7342f1b2838c996ebf5632920b8fb
Santiago Gutierrez <santigutierrez0001@gmail.com>
2022-07-06 20:53:47 -0500

    logica de agregar usuario funcional

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ app.get('/login', (req, res) => {
     res.render('login/login');
 });
 
+app.post('/registrar', async (req, res) => {
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const rol = req.body.rol;
+    const user = req.body.username;
+    const pass = req.body.pass;
+    let passwordHassh = await bcryptjs.hash(pass, 8);
+    conection.query("INSERT INTO usuarios SET ?", {nombre:nombre, apellido:apellido, usuario:user, pass:passwordHassh}, async(error, result) => {
+        if (error){
+            console.log(error);
+        } else {
+            res.render('login/login', {
+                alert: true,
+                alertTitle: "Registro",
+                alertMessage: "Registro Correcto",
+                alertIcon: "success",
+                showConfirmButton: true,
+                timer: 2000,
+                ruta: 'login'
+            });
+        }
+    });
+});
+
 app.listen(5000, (req, res) => {
     console.log("Servidor corriendo en https://localhost:5000");
 });
